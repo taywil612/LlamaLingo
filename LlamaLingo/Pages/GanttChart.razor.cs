414943df25d471e51f96f3e9b3db116302d64ba8
woodsj1206 <47829885+woodsj1206@users.noreply.github.com>
2023-12-05 23:32:56 -0500

    Fixed Gantt Chart

--- LlamaLingo/Pages/GanttChart.razor.cs
+++ LlamaLingo/Pages/GanttChart.razor.cs
@@ -5,22 +5,22 @@ namespace LlamaLingo.Pages
 {
 	public partial class GanttChart
     {
-        IEnumerable<LlamaLingo.Models.Task> TaskList;
+        IEnumerable<LlamaLingo.Models.Gantt> GanttList;
 
         protected override void OnInitialized()
         {
-            TaskList = db.Set<LlamaLingo.Models.Task>().ToList();
+            GanttList = db.Set<LlamaLingo.Models.Gantt>().ToList();
         }
 
-        public string[] Searchfields = new string[]
-        {
-            "TaskId",
-            "TaskLabel",
-            "TaskStartDate",
-            "TaskFinishDate",
-            "TaskDuration",
-            "TaskProgress",
-            "ParentIdFk"
+        public string[] Searchfields = new string[] { 
+            "GanttId", 
+            "GanttLabel", 
+            "GanttStartDate", 
+            "GanttFinishDate", 
+            "GanttDuration", 
+            "GanttProgress", 
+            "ParentIdFk" 
         };
+
     }
 }
