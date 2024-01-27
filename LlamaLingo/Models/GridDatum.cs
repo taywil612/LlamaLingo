fbc9726d18d0ba4aa707ac2bd9ab8782c7e7ff01
woodsj1206 <47829885+woodsj1206@users.noreply.github.com>
2024-01-27 18:27:39 -0500

    UI Updates
    - Created CSS variables for frequently used colors.
    - Fixed Chat Fab so that it color stays as a linear gradient.
    - Added hover animation to Chat Fab.
    - Small update to the design of the sidebars.
    - Fixed Chat input form so that it prompts the user to fill out both username and message field before being able to send a message.

---
+++ LlamaLingo/Models/GridDatum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LlamaLingo.Models;
+
+public partial class GridDatum
+{
+    public int GridTitleFk { get; set; }
+
+    public string GridDataLabel { get; set; }
+
+    public DateTime GridDataDate { get; set; }
+
+    public string GridDataType { get; set; }
+
+    public int GridDataValue { get; set; }
+
+    public string GridDataRadial { get; set; }
+
+    public short GridDataRadial1 { get; set; }
+}
---
+++ LlamaLingo/Models/GridRadial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LlamaLingo.Models;
+
+public partial class GridRadial
+{
+    public string PodLabel { get; set; }
+
+    public int PodIdFk { get; set; }
+
+    public int GridTitleId { get; set; }
+
+    public string GridTitleLabel64 { get; set; }
+
+    public string GridDataLabel { get; set; }
+
+    public DateTime GridDataDate { get; set; }
+
+    public string GridDataType { get; set; }
+
+    public int GridDataValue { get; set; }
+
+    public string GridDataRadial { get; set; }
+
+    public short GridDataRadial1 { get; set; }
+}
