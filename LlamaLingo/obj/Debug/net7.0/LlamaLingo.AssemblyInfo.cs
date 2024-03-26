adeccebda63089c6171f495b1b1d60d53c8f5a10
Chance25 <Chance2009@live.com>
2024-03-26 09:04:44 -0400

    Uploaded User Buttons

--- LlamaLingo/obj/Debug/net7.0/LlamaLingo.AssemblyInfo.cs
+++ LlamaLingo/obj/Debug/net7.0/LlamaLingo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LlamaLingo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70634cd6a2354359c68bb0ba8c3b3b929e7efb80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LlamaLingo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LlamaLingo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
