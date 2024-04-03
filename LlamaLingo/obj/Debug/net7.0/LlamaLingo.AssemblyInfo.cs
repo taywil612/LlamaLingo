ff5985f842f00307de94008c69eb889e7053aea3
Chance25 <Chance2009@live.com>
2024-04-03 16:04:14 -0400

    Revert "Bad"
    This reverts commit b6bab4c7b3fc8b77d4ed4f8f5ed33b141a3c1837.

--- LlamaLingo/obj/Debug/net7.0/LlamaLingo.AssemblyInfo.cs
+++ LlamaLingo/obj/Debug/net7.0/LlamaLingo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LlamaLingo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f594cdfaafe791a99132edc1066c8ccbf8542439")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LlamaLingo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LlamaLingo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
