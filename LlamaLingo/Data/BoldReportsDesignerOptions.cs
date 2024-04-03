11c9a1153601cb47c140dcf0b348d161cc75d343
taywil612 <114127446+taywil612@users.noreply.github.com>
2024-04-03 11:20:46 -0400

    Added Initial BoldReport
    Not fully implemented, serviceURL most likely needed!

---
+++ LlamaLingo/Data/BoldReportsDesignerOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace LlamaLingo.Data
+{
+    public class BoldReportsDesignerOptions
+    {
+        public string ServiceURL { get; set; }
+    }
+}
